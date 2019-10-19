f1e7b3d0bc06cba33d64f3b7cded854735d6efee
Anthony Platanios <e.a.platanios@gmail.com>
2019-10-19 19:10:17 -0400

    [LAYERS] Added an implementation of multi-head attention.

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "NCA",
+  platforms: [.macOS(.v10_12)],
+  products: [
+    .library(name: "NCA", targets: ["NCA"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.4.0"),
+    .package(url: "https://github.com/jkandzi/Progress.swift", from: "0.4.0"),
+    .package(url: "https://github.com/weichsel/ZIPFoundation/", .branch("master")),
+  ],
+  targets: [
+    .target(name: "NCA", dependencies: ["Logging", "Progress", "ZIPFoundation"]),
+    .testTarget(name: "NCATests", dependencies: ["NCA"])
+  ]
+)
